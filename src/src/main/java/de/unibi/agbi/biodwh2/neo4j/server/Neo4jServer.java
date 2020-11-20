4caba0f656030a7683cea27357ca776f979f4519
Marcel Friedrichs <mfriedrichs@techfak.uni-bielefeld.de>
2020-11-20 12:47:48 +0100

    Implement update check and logging if new version is available

--- src/src/main/java/de/unibi/agbi/biodwh2/neo4j/server/Neo4jServer.java
+++ src/src/main/java/de/unibi/agbi/biodwh2/neo4j/server/Neo4jServer.java
@@ -1,6 +1,11 @@
 package de.unibi.agbi.biodwh2.neo4j.server;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import de.unibi.agbi.biodwh2.core.model.Version;
 import de.unibi.agbi.biodwh2.neo4j.server.model.CmdArgs;
+import de.unibi.agbi.biodwh2.neo4j.server.model.GithubAsset;
+import de.unibi.agbi.biodwh2.neo4j.server.model.GithubRelease;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,12 +13,18 @@ import picocli.CommandLine;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.Optional;
+import java.util.jar.Manifest;
 
 public class Neo4jServer {
     private static final Logger LOGGER = LoggerFactory.getLogger(Neo4jServer.class);
+    private static final String BIODWH2_NEO4J_RELEASE_URL = "https://api.github.com/repos/BioDWH2/BioDWH2-Neo4j-Server/releases";
 
     private Neo4jServer() {
     }
@@ -31,6 +42,7 @@ public class Neo4jServer {
     }
 
     private void run(final CmdArgs commandLine) {
+        checkForUpdate();
         if (commandLine.createStart != null)
             createAndStartWorkspaceServer(commandLine);
         else if (commandLine.start != null)
@@ -77,7 +89,8 @@ public class Neo4jServer {
             LOGGER.info("Updating workspace neo4j cache checksum...");
         final Path hashFilePath = Paths.get(workspacePath, "neo4j/checksum.txt");
         try {
-            final String hash = HashUtils.getFastPseudoHashFromFile(Paths.get(workspacePath, "sources/mapped.db").toString());
+            final String hash = HashUtils.getFastPseudoHashFromFile(
+                    Paths.get(workspacePath, "sources/mapped.db").toString());
             final FileWriter writer = new FileWriter(hashFilePath.toFile());
             writer.write(hash);
             writer.close();
@@ -104,7 +117,8 @@ public class Neo4jServer {
 
     private boolean checkNeo4jDatabaseMatchesWorkspace(final String workspacePath) {
         try {
-            final String hash = HashUtils.getFastPseudoHashFromFile(Paths.get(workspacePath, "sources/mapped.db").toString());
+            final String hash = HashUtils.getFastPseudoHashFromFile(
+                    Paths.get(workspacePath, "sources/mapped.db").toString());
             final Path hashFilePath = Paths.get(workspacePath, "neo4j/checksum.txt");
             if (Files.exists(hashFilePath)) {
                 final String storedHash = new String(Files.readAllBytes(hashFilePath)).trim();
@@ -128,5 +142,55 @@ public class Neo4jServer {
         service.startNeo4jService(commandLine.boltPort);
         service.createDatabase();
         storeWorkspaceHash(workspacePath);
+    }
+
+    private void checkForUpdate() {
+        final Version currentVersion = getCurrentVersion();
+        Version mostRecentVersion = null;
+        String mostRecentDownloadUrl = null;
+        final ObjectMapper mapper = new ObjectMapper();
+        try {
+            final URL releaseUrl = new URL(BIODWH2_NEO4J_RELEASE_URL);
+            final List<GithubRelease> releases = mapper.readValue(releaseUrl, new TypeReference<List<GithubRelease>>() {
+            });
+            for (final GithubRelease release : releases) {
+                final Version version = Version.tryParse(release.tagName.replace("v", ""));
+                if (version != null) {
+                    final String jarName = "BioDWH2-Neo4j-Server-" + release.tagName + ".jar";
+                    final Optional<GithubAsset> jarAsset = release.assets.stream().filter(
+                            asset -> asset.name.equalsIgnoreCase(jarName)).findFirst();
+                    if (jarAsset.isPresent() && mostRecentVersion == null || version.compareTo(mostRecentVersion) > 0) {
+                        mostRecentVersion = version;
+                        //noinspection OptionalGetWithoutIsPresent
+                        mostRecentDownloadUrl = jarAsset.get().browserDownloadUrl;
+                    }
+                }
+            }
+        } catch (IOException | ClassCastException ignored) {
+        }
+        if (currentVersion == null && mostRecentVersion != null || currentVersion != null && currentVersion.compareTo(
+                mostRecentVersion) < 0) {
+            LOGGER.info("=======================================");
+            LOGGER.info("New version " + mostRecentVersion + " of BioDWH2-Neo4j-Server is available at:");
+            LOGGER.info(mostRecentDownloadUrl);
+            LOGGER.info("=======================================");
+        }
+    }
+
+    private Version getCurrentVersion() {
+        try {
+            final Enumeration<URL> resources = getClass().getClassLoader().getResources("META-INF/MANIFEST.MF");
+            while (resources.hasMoreElements()) {
+                try {
+                    final Manifest manifest = new Manifest(resources.nextElement().openStream());
+                    final Version version = Version.tryParse(manifest.getMainAttributes().getValue("BioDWH2-version"));
+                    if (version != null)
+                        return version;
+                } catch (IOException ignored) {
+                }
+            }
+        } catch (IOException ignored) {
+        }
+        return null;
     }
 }
