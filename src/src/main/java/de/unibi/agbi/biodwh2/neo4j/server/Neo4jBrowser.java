b5ccad34bb6f8a9a2e2dc4f4dd3c1c4eef136f39
Marcel Friedrichs <astror.enales@gmail.com>
2021-03-01 13:28:20 +0100

    Fix neo4j-browser download

--- src/src/main/java/de/unibi/agbi/biodwh2/neo4j/server/Neo4jBrowser.java
+++ src/src/main/java/de/unibi/agbi/biodwh2/neo4j/server/Neo4jBrowser.java
@@ -19,12 +19,16 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Paths;
+import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 final class Neo4jBrowser {
     private static final Logger LOGGER = LoggerFactory.getLogger(Neo4jBrowser.class);
     private static final String NEO4J_BROWSER_RELEASE_URL = "https://api.github.com/repos/neo4j/neo4j-browser/releases";
-    private static final String FALLBACK_DOWNLOAD_URL = "https://github.com/neo4j/neo4j-browser/releases/download/4.1.0/neo4j-browser-4.1.0.tgz";
+    private static final String FALLBACK_DOWNLOAD_URL = "https://github.com/neo4j/neo4j-browser/releases/download/4.1.3/neo4j-browser-4.1.3.tgz";
+    private static final String BROWSER_ARCHIVE_FILE_NAME = "neo4j-browser.tgz";
 
     private final String neo4jPath;
     private final String browserDistPath;
@@ -35,38 +39,50 @@ final class Neo4jBrowser {
     }
 
     public boolean downloadNeo4jBrowser() {
-        final File browserArchiveFile = Paths.get(neo4jPath, "neo4j-browser.tgz").toFile();
+        final File browserArchiveFile = Paths.get(neo4jPath, BROWSER_ARCHIVE_FILE_NAME).toFile();
         if (!browserArchiveFile.exists()) {
-            final String downloadUrl = getNeo4jBrowserDownloadUrl();
-            if (downloadUrl == null)
-                return false;
-            try {
-                FileUtils.copyURLToFile(new URL(downloadUrl), browserArchiveFile);
-            } catch (IOException e) {
-                if (LOGGER.isErrorEnabled())
-                    LOGGER.error("Failed to download neo4j-browser.", e);
-                return false;
+            if (LOGGER.isInfoEnabled())
+                LOGGER.info("Downloading neo4j-browser...");
+            boolean foundSuitableVersion = false;
+            for (final String downloadUrl : getNeo4jBrowserDownloadUrlCandidates()) {
+                try {
+                    FileUtils.copyURLToFile(new URL(downloadUrl), browserArchiveFile);
+                    final boolean extractedSuccessful = extractNeo4jBrowserArchive(browserArchiveFile);
+                    if (extractedSuccessful && Paths.get(browserDistPath).toFile().exists()) {
+                        foundSuitableVersion = true;
+                        if (LOGGER.isInfoEnabled())
+                            LOGGER.info("Suitable neo4j-browser version found: " + downloadUrl);
+                        break;
+                    }
+                } catch (IOException ignored) {
+                }
             }
+            if (!foundSuitableVersion) {
+                if (LOGGER.isErrorEnabled())
+                    LOGGER.error("Failed to download neo4j-browser.");
+            }
+            return foundSuitableVersion;
         }
         return extractNeo4jBrowserArchive(browserArchiveFile);
     }
 
-    private String getNeo4jBrowserDownloadUrl() {
-        ObjectMapper mapper = new ObjectMapper();
+    private String[] getNeo4jBrowserDownloadUrlCandidates() {
+        final TypeReference<List<GithubRelease>> releaseListType = new TypeReference<List<GithubRelease>>() {
+        };
+        final Set<String> candidates = new HashSet<>();
+        candidates.add(FALLBACK_DOWNLOAD_URL);
+        final ObjectMapper mapper = new ObjectMapper();
         try {
             final URL neo4jBrowserRelease = new URL(NEO4J_BROWSER_RELEASE_URL);
-            List<GithubRelease> releases = mapper.readValue(neo4jBrowserRelease,
-                                                            new TypeReference<List<GithubRelease>>() {
-                                                            });
+            final List<GithubRelease> releases = mapper.readValue(neo4jBrowserRelease, releaseListType);
             for (GithubRelease release : releases)
                 if (release.assets != null && release.assets.size() > 0)
-                    return release.assets.get(0).browserDownloadUrl;
-            return FALLBACK_DOWNLOAD_URL;
+                    candidates.add(release.assets.get(0).browserDownloadUrl);
         } catch (IOException | ClassCastException e) {
             if (LOGGER.isErrorEnabled())
                 LOGGER.error("Failed to retrieve neo4j-browser download url.", e);
-            return null;
         }
+        return candidates.stream().sorted(Comparator.reverseOrder()).toArray(String[]::new);
     }
 
     private boolean extractNeo4jBrowserArchive(final File browserArchiveFile) {
